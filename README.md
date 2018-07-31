d3681806313f9f64d143d7533aeffaa8b8cd1f4c
etsxxx <ets.nkn@gmail.com>
2018-07-31 16:19:33 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Userscripts for Tampermonkey or Greasemonkey.
+
+## Before Installation
+
+### Setup your browser
+Make sure you have user scripts enabled in your browser.
+
+	* Chrome - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=chrome).
+	* Safari - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=safari).
+	* Firefox - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=firefox) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+
+### Read Limitation
+The author tested userscripts with limited browser versions, limited time.
+PR is welcome.
+
+## Get information and install userscripts
+
+### Slack Code Syntax Highlight
+
+* Created: 2018/07/31
+* Updated: 2018/07/31
+* Enabled URI: https://*.slack.com/messages/*
+* Information
+    * Add syntax highlight to code block by using [google-code-prettify](https://github.com/google/code-prettify)
+    * Language mode is 'bsh' (You can change it simply)
+
+[Install this userscript](https://raw.githubusercontent.com/etsxxx/userscripts/master/src/slack-code-syntax.js)
+
---
+++ src/slack-code-syntax-highlight.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Slack Code Syntax Highlight
+// @version      0.1
+// @description  auto syntax highlighting at slack code block
+// @author       etsxxx
+// @match       https://*.slack.com/messages/*
+// @grant        none
+// @require     https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js
+// @updateURL   https://raw.githubusercontent.com/etsxxx/userscripts/master/src/slack-code-syntax.js
+// @downloadURL https://raw.githubusercontent.com/etsxxx/userscripts/master/src/slack-code-syntax.js
+// ==/UserScript==
+
+function initPrettyPrint() {
+    var styleElement = document.createElement('style');
+    styleElement.innerHTML += "li.L0, li.L2, li.L4, li.L6, li.L8 {background-color: #ffffff; list-style-type: decimal !important;}";
+    styleElement.innerHTML += "li.L1, li.L3, li.L5, li.L7, li.L9 {background-color: #fbfbfb; list-style-type: decimal !important;}";
+    document.head.appendChild(styleElement);
+}
+
+
+function addPrettyPrintTags() {
+    let preElements = document.getElementsByClassName('c-mrkdwn__pre');
+    var pres = Array.prototype.slice.call(preElements);
+    pres.forEach(anchorElement => {
+        anchorElement.classList.add('prettyprint');
+        anchorElement.classList.add('lang-bsh');
+        anchorElement.classList.add('linenums');
+    });
+
+    // thread
+    preElements = document.getElementsByClassName('special_formatting');
+    pres = Array.prototype.slice.call(preElements);
+    pres.forEach(anchorElement => {
+        anchorElement.classList.add('prettyprint');
+        anchorElement.classList.add('lang-bsh');
+        anchorElement.classList.add('linenums');
+    });
+
+    PR.prettyPrint();
+}
+
+
+$(function($) {
+    'use strict';
+    // On page load
+    initPrettyPrint();
+    addPrettyPrintTags();
+
+    // When new post is added
+    $("#client_body").on('DOMSubtreeModified propertychange', addPrettyPrintTags);
+});
